d17a714153deebb7a4dd826e5d914249e3db30d7
Satya Arjunan <S.Arjunan@victorchang.edu.au>
2022-06-20 18:10:15 +1000

    rename software

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 #
-#        This file is part of Motocyte
+#        This file is part of Spellor
 #
-#        Copyright (C) 2019 Satya N.V. Arjunan
+#        Copyright (C) 2022 Satya N.V. Arjunan
 #
 #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 #
 #
-# Motocyte is free software; you can redistribute it and/or
+# Spellor is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public
 # License as published by the Free Software Foundation; either
 # version 2 of the License, or (at your option) any later version.
 # 
-# Motocyte is distributed in the hope that it will be useful,
+# Spellor is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public
-# License along with Motocyte -- see the file COPYING.
+# License along with Spellor -- see the file COPYING.
 # If not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 # 
@@ -45,7 +45,7 @@ class MainWindow(QMainWindow):
     def initialise_ui(self):
         self.statusbar = self.statusBar()
         self.statusbar.showMessage("Ready")
-        self.setWindowTitle("Aritya Spelling Bee")
+        self.setWindowTitle("Aritya's Spellor")
         self.setWindowIcon(QIcon(QApplication.style().standardIcon(
           QStyle.SP_FileDialogListView)))
         self.show()
